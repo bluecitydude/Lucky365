e0f314caab174714ceef949036fe0b070c0db03f
TejendraPurohit <167332577+bluecitydude@users.noreply.github.com>
2026-01-19 10:29:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const hackathonTeams = [
     {
         teamName: "ResTecSol",
         members: "Tejendra Purohit, Jasleen Kaur Handa, Shahid Ansari, Tushar Sharma",
-        projectName: "Resource & Task Management System",
-        description: "A productivity and workflow solution for teams and organizations.",
+        projectName: "Talent Management System",
+        description: "Find your talent and improve it in simple ways.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect><path d="M9 14l2 2 4-4"></path><path d="M9 19h6"></path><path d="M9 10h6"></path></svg>`, // Checklist
         link: "https://tms-restecsol.lovable.app/auth"
     },
